2a46f38892097f5530a0e8e784c26f92048ccb6b
Gabriel Penteado <gabripenteado@gmail.com>
2024-07-12 23:12:43 -0300

    created unit tests and doc the api with swagger

--- src/test/java/com/api_java_spring_mongodb/api_java_spring_mongodb/ApiJavaSpringMongodbApplicationTests.java
+++ src/test/java/com/api_java_spring_mongodb/api_java_spring_mongodb/ApiJavaSpringMongodbApplicationTests.java
@@ -1,13 +1,36 @@
 package com.api_java_spring_mongodb.api_java_spring_mongodb;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import com.api_java_spring_mongodb.api_java_spring_mongodb.entity.CategoryEntity;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ApiJavaSpringMongodbApplicationTests {
+
+	@Autowired
+	private WebTestClient webTestClient;
 
 	@Test
 	void testCreateCategorySuccess() {
+		CategoryEntity newCategory = new CategoryEntity();
+
+		newCategory.setTitle("testCatTitle");
+		newCategory.setDescription("testCatDesc");
+
+		webTestClient
+				.post()
+				.uri("/api/category")
+				.bodyValue(newCategory)
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody()
+				.jsonPath("$.title").isEqualTo(newCategory.getTitle())
+				.jsonPath("$.description").isEqualTo(newCategory.getDescription())
+				.returnResult();
 	}
 
 }
